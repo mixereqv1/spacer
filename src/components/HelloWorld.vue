4559125682d3987416778a46182089d222065443
mixereqv1 <mateuszral7@gmail.com>
2019-12-30 21:06:19 +0100

    Add vue config

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1 class="title">Hello world</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.title {
+  text-align: center;
+  font-size: 5em;
+  font-family: sans-serif;
+}
+</style>
